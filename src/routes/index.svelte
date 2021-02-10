6ba7ce36addbe1533566dd801234e6d7c312afd0
azukiazusa1 <cycleippei@gmail.com>
2021-02-10 22:37:37 +0900

    tailwind css

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,45 +3,11 @@
 	console.log(process.env.API_KEY)
 </script>
 
-<style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
-</style>
-
 <svelte:head>
 	<title>Sapper project template</title>
 </svelte:head>
 
-<h1>Great success!</h1>
+<h1 class="text-blue-700">Great success!</h1>
 
 <figure>
 	<img alt="Success Kid" src="{successkid}">
