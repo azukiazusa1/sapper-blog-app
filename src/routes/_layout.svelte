acc0b67f3cbeca1b71d4e57ab007c1a4e7fee2f5
azukiazusa1 <cycleippei@gmail.com>
2021-02-11 09:35:07 +0900

    toggle dark theme

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,22 +1,40 @@
 <script lang="ts">
+import { onMount } from 'svelte';
+
 	import Nav from '../components/Nav.svelte';
 
 	export let segment: string;
+	let body;
+	let darkMode = false
+
+	onMount(() => {
+		body = document.body
+		if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+			document.documentElement.classList.add('dark')
+		} else {
+			document.documentElement.classList.remove('dark')
+		}
+	})
+	$: {
+			if (body) {
+			if (darkMode) {
+				body.classList.add('dark')
+				localStorage.theme = 'dark'
+			} else {
+				body.classList.remove('dark')
+				localStorage.theme = 'light'
+			}
+		}
+	}
+
+	const toggleDarkMode = () => {
+		darkMode = !darkMode
+	}
 </script>
 
-<style>
-	main {
-		position: relative;
-		max-width: 56em;
-		background-color: white;
-		padding: 2em;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-</style>
-
+<button on:click={toggleDarkMode}>button</button>
 <Nav {segment}/>
 
-<main>
+<main class="bg-white dark:bg-black">
 	<slot></slot>
 </main>
