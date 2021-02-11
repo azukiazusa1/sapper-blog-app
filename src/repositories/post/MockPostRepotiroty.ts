85ac4707d7f0980fdee62f9b6a661d286c6c3ac3
azukiazusa1 <cycleippei@gmail.com>
2021-02-11 17:49:37 +0900

    createdummyposts

--- src/repositories/post/MockPostRepotiroty.ts
+++ src/repositories/post/MockPostRepotiroty.ts
@@ -1,18 +1,11 @@
+import { createDummyPosts, createDummyPost } from "../../utils/createDummyPosts";
 import type { PostRepositoryInterFace } from "./types";
 
 export class MockPostRepository implements PostRepositoryInterFace {
   get() {
-    return Promise.resolve([{
-      slug: '1',
-      title: 'title1',
-      about: 'lorem ipsum',
-    }])
+    return Promise.resolve(createDummyPosts(15))
   }
   find(id: string) {
-    return Promise.resolve({
-      slug: '1',
-      title: 'title1',
-      about: 'lorem ipsum',
-    })
+    return Promise.resolve(createDummyPost(id))
   }
 }
---
+++ src/utils/createDummyPosts.ts
@@ -0,0 +1,17 @@
+import type { Post } from "../repositories/post"
+
+export const createDummyPost = (id = '1') => {
+  return {
+    slug: id,
+    title: `title${id}`,
+    about: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum perspiciatis quasi cum illo nihil dolorum eaque itaque quaerat deleniti tempore! Fuga animi maxime culpa nisi veritatis? Quasi officia temporibus voluptates?'
+  } as Post
+}
+
+export const createDummyPosts = (num: number) => {
+  const dummyPosts: Post[] = []
+  for (let i = 0; i < num; i++) {
+    dummyPosts.push(createDummyPost(String(i)))
+  }
+  return dummyPosts
+}
