707c7be1d6a46c5130849d8fb6ef9fb2974d3dfe
azukiazusa1 <cycleippei@gmail.com>
2021-02-12 15:35:35 +0900

    tag component

---
+++ src/components/Tag.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let name: string
+  export let slug: string
+</script>
+
+<a 
+  class="inline-block bg-gray-200 rounded-full px-3 py-1 dark:bg-gray-500 text-xs font-semibold mr-2 mb-2"
+  href={`tags/${slug}`}
+>
+  {name}
+</a>
