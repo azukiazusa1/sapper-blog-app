7b12a9a4fd198ce8745ee7ea44e13244e8e4a16a
azukiazusa1 <cycleippei@gmail.com>
2021-02-23 11:05:14 +0900

    loading

--- src/components/DropDownMenu.svelte
+++ src/components/DropDownMenu.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import DropDownItem from './DropDownItem.svelte'
+  import Loading from './Icons/Loading.svelte'
 
   export let items: any[]
   export let itemHref = 'href'
@@ -8,9 +9,11 @@
   export let loading = false
 </script>
 
-<div class="absolute z-20 overflow-hidden bg-white rounded-md shadow-lg w-80 dark:bg-gray-700">
+<div class="absolute z-20 overflow-hidden bg-white rounded-md shadow-lg w-80 max-h-60 overflow-y-scroll dark:bg-gray-700">
   {#if loading}
-    <div class="py-2">loading...</div>
+    <div class="py-2 text-center">
+      <Loading />
+    </div>
   {:else}
     <div class="py-2">
       {#each items as item, index (index)}
