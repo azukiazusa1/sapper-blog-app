a1765ceba9b0641a7faee280f0fe682974163ff0
azukiazusa1 <cycleippei@gmail.com>
2021-02-25 21:52:07 +0900

    fix create dummy posts

--- src/repositories/post/MockPostRepotiroty.ts
+++ src/repositories/post/MockPostRepotiroty.ts
@@ -1,12 +1,13 @@
+import type { PostsQueryVariables } from "../../generated/graphql";
 import { createDummyPosts, createDummyPost, createDummyPostBySlugQuery } from "../../utils/createDummyPosts";
 import type { PostRepositoryInterFace } from "./types";
 
 export class MockPostRepository implements PostRepositoryInterFace {
-  get(_) {
-    return Promise.resolve(createDummyPosts(15))
+  get(queryVariables: PostsQueryVariables) {
+    return Promise.resolve(createDummyPosts(100, queryVariables.limit, queryVariables.skip)())
   }
   search(_) {
-    return Promise.resolve(createDummyPosts(15))
+    return Promise.resolve(createDummyPosts(15)())
   }
   find(slug: string) {
     return Promise.resolve(createDummyPostBySlugQuery(slug))
