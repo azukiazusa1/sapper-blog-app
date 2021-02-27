852879d5dbde4880f515e7b66986143f256edc29
azukiazusa1 <cycleippei@gmail.com>
2021-02-27 21:35:28 +0900

    className props

--- src/components/Icons/Menu.svelte
+++ src/components/Icons/Menu.svelte
@@ -1,4 +1,8 @@
-<svg on:click xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+<script lang="ts">
+  export let className = ''
+</script>
+
+<svg class={className} on:click xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
   <path
     fill-rule="evenodd"
     d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
