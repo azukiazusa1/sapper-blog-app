bcadc1a82050f4f718912ce60527049bad4e4438
azukiazusa1 <cycleippei@gmail.com>
2021-03-03 22:25:51 +0900

    ogp component

---
+++ src/components/Ogp.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  export let title = ''
+  export let type = ''
+  export let url = ''
+  export let image = ''
+  export let description = ''
+  export let card = 'summary'
+</script>
+
+<svelte:head>
+  <meta property="og:site_name" content="azukiazusaのテックブログ2" />
+  <meta property="og:title" content={title} />
+  <meta property="og:url" content={url} />
+  <meta property="og:image" content={image} />
+  <meta property="og:description" content={description} />
+  <meta name="twitter:card" content={card} />
+</svelte:head>
