218a8441ef0ac900e4b3be43df9b00f5765bdf51
azukiazusa1 <cycleippei@gmail.com>
2021-03-03 22:37:16 +0900

    user ogp

--- src/routes/blog/[slug].svelte
+++ src/routes/blog/[slug].svelte
@@ -13,21 +13,33 @@
 </script>
 
 <script lang="ts">
+  import { stores } from '@sapper/app';
   import Card from '../../components/Card.svelte'
+  import Ogp from '../../components/Ogp.svelte'
   import PostList from '../../components/PostList.svelte'
   import type { BlogPost } from '../../generated/graphql'
 
+  const { page } = stores()
+
   export let post: Pick<
     BlogPost,
-    'title' | 'slug' | 'about' | 'article' | 'createdAt' | 'tagsCollection' | 'relatedArticleCollection'
+    'title' | 'slug' | 'about' | 'article' | 'createdAt' | 'tagsCollection' | 'relatedArticleCollection' | 'thumbnail'
   >
   export let contents: string
+
+  $: protocol = process.env.NODE_ENV === 'production' ? 'https' : 'http'
 </script>
 
 <svelte:head>
   <title>{post.title}</title>
   <meta name="description" content={post.about} />
 </svelte:head>
+
+<Ogp title={post.title}
+  description={post.about}
+  url={`${protocol}://${$page.host}${$page.path}`}
+  image={post.thumbnail.url}
+/>
 
 <div class="my-12">
   <Card title={post.title} tags={post.tagsCollection.items} createdAt={post.createdAt} {contents} />
