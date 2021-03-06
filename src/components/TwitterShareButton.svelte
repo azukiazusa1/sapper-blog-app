e4a92078d38589906c235dd5138d270dd87cba8a
azukiazusa1 <cycleippei@gmail.com>
2021-03-06 21:18:14 +0900

    twitter share button

---
+++ src/components/TwitterShareButton.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import TwitterIcon from './Icons/Twitter.svelte'
+  export let url: string
+  export let text: string
+</script>
+
+<TwitterIcon className="h-6 w-6" href={`https://twitter.com/share?url=${url}&text=${text}`} target="_blank" />
