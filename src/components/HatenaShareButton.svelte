086a636063b7cd12aef264320711c2d77f2823a2
azukiazusa1 <cycleippei@gmail.com>
2021-03-29 23:27:24 +0900

    hatena share button

---
+++ src/components/HatenaShareButton.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  export let url: string
+  export let text: string
+
+  $: href = `https://b.hatena.ne.jp/add?mode=confirm&url=${url}&title=${text}`
+</script>
+
+<a 
+  {href}
+  target="_blank" 
+  rel="noopener" 
+>
+  <img src="https://b.st-hatena.com/images/v4/public/entry-button/button-only.png" alt="Hatena" class="h-6 w-6">
+</a>
