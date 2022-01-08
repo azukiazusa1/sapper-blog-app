893d3dd0c64544de8790411d9af07450bac4a17e
azukiazusa1 <cycleippei@gmail.com>
2022-01-08 21:42:04 +0900

    svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,67 +1,18 @@
 import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// options passed to svelte.compile (https://svelte.dev/docs#svelte_compile)
-	compilerOptions: null,
-
-	// an array of file extensions that should be treated as Svelte components
-	extensions: ['.svelte'],
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
 
 	kit: {
 		adapter: adapter(),
-		amp: false,
-		appDir: '_app',
-		files: {
-			assets: 'static',
-			hooks: 'src/hooks',
-			lib: 'src/lib',
-			routes: 'src/routes',
-			serviceWorker: 'src/service-worker',
-			template: 'src/app.html'
-		},
-		floc: false,
-		headers: {
-			host: null,
-			protocol: null
-		},
-		host: null,
-		hydrate: true,
-		package: {
-			dir: 'package',
-			emitTypes: true,
-			// excludes all .d.ts and files starting with _ as the name
-			exports: (filepath) => !/^_|\/_|\.d\.ts$/.test(filepath),
-			files: () => true
-		},
-    paths: {
-			assets: '',
-			base: ''
-		},
-		prerender: {
-			concurrency: 1,
-			crawl: true,
-			enabled: true,
-			entries: ['*'],
-			onError: 'fail'
-		},
-		protocol: null,
-		router: true,
-		serviceWorker: {
-			register: true,
-			files: (filepath) => !/\.DS_STORE/.test(filepath)
-		},
-		ssr: true,
-		target: null,
-		trailingSlash: 'never',
-		vite: () => ({})
-	},
 
-	// SvelteKit uses vite-plugin-svelte. Its options can be provided directly here.
-	// See the available options at https://github.com/sveltejs/vite-plugin-svelte/blob/main/docs/config.md
-
-	// options passed to svelte.preprocess (https://svelte.dev/docs#svelte_preprocess)
-	preprocess: null
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte'
+	}
 };
 
 export default config;
