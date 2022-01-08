52201fb33e65a469bfd04b2008620b1a3ed197c2
azukiazusa1 <cycleippei@gmail.com>
2022-01-09 08:37:32 +0900

    format

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,33 +1,20 @@
 module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  plugins: ['svelte3', '@typescript-eslint'],
+  ignorePatterns: ['*.cjs'],
+  overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+  settings: {
+    'svelte3/typescript': () => require('typescript'),
+  },
   parserOptions: {
+    sourceType: 'module',
     ecmaVersion: 2020,
-    sourceType: 'module'
   },
   env: {
-    es6: true,
     browser: true,
-    node: true
+    es2017: true,
+    node: true,
   },
-  extends: [
-    'eslint:recommended',
-    'prettier'
-  ],
-  plugins: [
-    'svelte3'
-  ],
-  ignorePatterns: [
-    'public/build/'
-  ],
-  overrides: [
-    {
-      files: ['**/*.svelte'],
-      processor: 'svelte3/svelte3'
-    }
-  ],
-  rules: {
-    'no-var': 'error'
-  },
-  settings: {
-    // Nothing
-  }
 }
