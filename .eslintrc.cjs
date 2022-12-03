efdf848490c049719ec70bccdff4a979d14adba1
azukiazusa1 <cycleippei@gmail.com>
2022-12-03 10:50:23 +0900

    ?

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,13 +15,13 @@ module.exports = {
   ],
   parserOptions: {
     sourceType: 'module',
-    ecmaVersion: 2020,
+    ecmaVersion: 2021,
     project: './tsconfig.json',
     extraFileExtensions: ['.svelte'], // This is a required setting in `@typescript-eslint/parser` v4.24.0.
   },
   env: {
     browser: true,
-    es2017: true,
+    es2021: true,
     node: true,
   },
 }
