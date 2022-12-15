e6600fd70408460e377b2eaf48ce45626b6b977d
azukiazusa1 <59350345+azukiazusa1@users.noreply.github.com>
2022-12-15 22:48:47 +0900

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static'
+import adapter from '@sveltejs/adapter-vercel'
 import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,10 +8,7 @@ const config = {
   preprocess: preprocess(),
 
   kit: {
-    adapter: adapter({
-      trailingSlash: 'always',
-      strict: false,
-    }),
+    adapter: adapter(),
   },
 }
 
