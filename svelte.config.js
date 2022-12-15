22ea17879136f6ecc525b45880b462ce1d708e83
azukiazusa1 <cycleippei@gmail.com>
2022-12-15 21:53:48 +0900

    test

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static'
+import adapter from '@sveltejs/adapter-vercel'
 import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,9 +8,7 @@ const config = {
   preprocess: preprocess(),
 
   kit: {
-    adapter: adapter({
-      trailingSlash: 'always',
-    }),
+    adapter: adapter(),
   },
 }
 
