d89b1444aa474637c9a0a2050e1e4b3e66ff8ca1
azukiazusa1 <cycleippei@gmail.com>
2022-12-15 22:18:50 +0900

    vercel adapter

--- src/routes/api/search/+server.ts
+++ src/routes/api/search/+server.ts
@@ -4,10 +4,19 @@ const PostRepository = RepositoryFactory[POST]
 
 export const prerender = false
 
+const paginateParams = (page: number) => {
+  const limit = 12
+  const skip = (page - 1) * limit
+  return { limit, skip }
+}
+
 export const GET: RequestHandler = async ({ request }) => {
   const url = new URL(request.url)
   const q = url.searchParams.get('q') ?? ''
+  const page = Number(url.searchParams.get('page') ?? 1)
 
-  const posts = await PostRepository.search({ q })
+  const { limit, skip } = paginateParams(page)
+
+  const posts = await PostRepository.search({ q, limit, skip })
   return json(posts)
 }
