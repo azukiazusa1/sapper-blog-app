d4320cf0783848750617f5dbabfb1a136db6660c
azukiazusa1 <cycleippei@gmail.com>
2023-02-15 09:51:13 +0900

    a

--- src/routes/drafts/+page.server.ts
+++ src/routes/drafts/+page.server.ts
@@ -2,8 +2,6 @@ import type { PageServerLoad } from './$types'
 import RepositoryFactory, { POST } from '../../repositories/RepositoryFactory'
 const PostRepository = RepositoryFactory[POST]
 
-export const prerender = false
-
 export const config = {
   isr: {
     expiration: 60,
@@ -11,12 +9,7 @@ export const config = {
 }
 
 export const load: PageServerLoad = async () => {
-  try {
-    const posts = await PostRepository.getAllPreview()
+  const posts = await PostRepository.getAllPreview()
 
-    return posts
-  } catch (e) {
-    console.error(e)
-    return []
-  }
+  return posts
 }
