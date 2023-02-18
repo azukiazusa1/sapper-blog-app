52f7a33e255d96ce2ee2c81bbb1bfd6c379f39ac
azukiazusa1 <59350345+azukiazusa1@users.noreply.github.com>
2023-02-18 15:07:37 +0900

    修正提案ボタン (#162)
    * 修正提案ボタン
    
    * update snapshot

---
+++ app/src/components/GitHubEditButton.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import GitHub from './Icons/GitHub.svelte'
+  import GitHubIcon from './Icons/GitHub.svelte'
+  export let slug: string
+</script>
+
+<a
+  href={`https://github.com/azukiazusa1/sapper-blog-app/tree/main/contents/blogPost/${slug}.md`}
+  target="_blank"
+  rel="noopener noreferrer"
+  class="inline-flex items-center justify-center px-4 py-2 text-base font-medium text-center text-gray-900 border border-gray-400 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-500 dark:hover:bg-gray-800 dark:focus:ring-gray-900"
+>
+  <GitHubIcon className="h6 w-6 mr-2" />
+  GitHub で修正を提案する
+</a>
