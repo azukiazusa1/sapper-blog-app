b7ff4bb47903d09ae81467b4acb637f022f8bb2d
azukiazusa1 <59350345+azukiazusa1@users.noreply.github.com>
2023-02-19 16:29:07 +0900

    close #185 svelte-kit-env (#186)
    * close #185 svelte-kit-env
    
    * PREVIEW API KEY
    
    * a

--- packages/content-management/src/env.ts
+++ packages/content-management/src/env.ts
@@ -2,22 +2,22 @@ import dotenv from 'dotenv'
 import findConfig from 'find-config'
 dotenv.config({ path: findConfig('.env') || '' })
 
-const { MANAGEMENT_TOKEN, VITE_SPACE, VITE_ENVIRONMENTS } = process.env
+const { MANAGEMENT_TOKEN, SPACE, ENVIRONMENTS } = process.env
 
 if (!MANAGEMENT_TOKEN) {
   throw new Error('.env に MANAGEMENT_TOKEN を設定してください。')
 }
 
-if (!VITE_SPACE) {
-  throw new Error('.env に VITE_SPACE を設定してください。')
+if (!SPACE) {
+  throw new Error('.env に SPACE を設定してください。')
 }
 
-if (!VITE_ENVIRONMENTS) {
-  throw new Error('.env に VITE_ENVIRONMENTS を設定してください。')
+if (!ENVIRONMENTS) {
+  throw new Error('.env に ENVIRONMENTS を設定してください。')
 }
 
 export const Env = {
   accessToken: MANAGEMENT_TOKEN,
-  space: VITE_SPACE,
-  environments: VITE_ENVIRONMENTS,
+  space: SPACE,
+  environments: ENVIRONMENTS,
 } as const satisfies Record<string, string>
