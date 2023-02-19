a2b046e5a814b0e48ed87a87753aa4ea902accf6
azukiazusa1 <59350345+azukiazusa1@users.noreply.github.com>
2023-02-19 18:02:55 +0900

    close #152 isr (#187)
    * close #152 isr
    
    * export const prerender = 'auto'
    
    * ?

--- app/src/routes/drafts/+page.server.ts
+++ app/src/routes/drafts/+page.server.ts
@@ -1,16 +1,17 @@
 import type { PageServerLoad } from './$types'
 import RepositoryFactory, { POST } from '../../repositories/RepositoryFactory'
+import type { Config } from '@sveltejs/adapter-vercel'
 const PostRepository = RepositoryFactory[POST]
 
-export const prerender = false
+export const prerender = 'auto'
+export const config = {
+  isr: {
+    expiration: 60,
+  },
+} satisfies Config
 
 export const load: PageServerLoad = async () => {
-  try {
-    const posts = await PostRepository.getAllPreview()
+  const posts = await PostRepository.getAllPreview()
 
-    return posts
-  } catch (e) {
-    console.error(e)
-    return []
-  }
+  return posts
 }
