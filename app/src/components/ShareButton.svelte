579ee42e2e6882137e3609446be338202da0a81b
azukiazusa1 <cycleippei@gmail.com>
2023-02-20 22:19:29 +0900

    browser

--- app/src/components/ShareButton.svelte
+++ app/src/components/ShareButton.svelte
@@ -1,11 +1,30 @@
 <script lang="ts">
   import ShareIcon from './Icons/Share.svelte'
+  import { browser } from '$app/environment'
+
+  export let title: string
+  export let text: string
+  export let url: string
+
+  const share = async () => {
+    if (!navigator.share) return
+    await navigator.share({ title, text, url }).catch(() => {
+      // noop
+    })
+  }
+
+  $: supportNavigationShare = browser && 'share' in navigator
 </script>
 
-<button
-  type="button"
-  class="group inline-flex items-center h-9 rounded-full text-sm font-semibold whitespace-nowrap px-3 focus:outline-none focus:ring-2 bg-gray-100 text-slate-900 hover:bg-gray-200 focus:ring-gray-500 dark:bg-gray-600 dark:text-gray-50 dark:hover:bg-gray-500 dark:focus:ring-gray-400"
->
-  <ShareIcon className="h-6 w-6 mr-2" />
-  <span>Share</span>
-</button>
+{#if supportNavigationShare}
+  <button
+    type="button"
+    class="group inline-flex items-center h-9 rounded-full text-sm font-semibold whitespace-nowrap px-3 focus:outline-none focus:ring-2 bg-gray-100 text-slate-900 hover:bg-gray-200 focus:ring-gray-500 dark:bg-gray-600 dark:text-gray-50 dark:hover:bg-gray-500 dark:focus:ring-gray-400"
+    on:click={share}
+  >
+    <ShareIcon className="h-6 w-6 mr-2" />
+    <span>Share</span>
+  </button>
+{:else}
+  <slot name="fallback" />
+{/if}
--- app/src/routes/blog/[slug]/+page.svelte
+++ app/src/routes/blog/[slug]/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import { page } from '$app/stores'
   import Card from '../../../components/Card/Card.svelte'
   import Ogp from '../../../components/Ogp.svelte'
   import PostList from '../../../components/PostList.svelte'
@@ -12,60 +11,43 @@
 
   export let data: PageData
 
-  const share = async () => {
-    if (!navigator.share) return
-    await navigator
-      .share({
-        title: data.post.title,
-        text: data.post.about,
-        url: `${variables.baseURL}/blog/${data.post.slug}`,
-      })
-      .catch(() => {
-        // noop
-      })
-  }
-
   $: post = data.post
   $: contents = data.contents
   $: tagNames = post.tagsCollection.items.map((t) => encodeURIComponent(t.name))
 
-  $: protocol = process.env.NODE_ENV === 'development' ? 'http' : 'https'
-  $: url = `${protocol}://${$page.url.host}${$page.url.pathname}`
-  $: supportNavigationShare = 'share' in navigator
+  $: url = `${variables.baseURL}/blog/${post.slug}`
 </script>
 
 <svelte:head>
   <title>{post.title}</title>
   <meta name="description" content={post.about} />
-  <link rel="canonical" href={`${variables.baseURL}/blog/${post.slug}`} />
+  <link rel="canonical" href={url} />
 </svelte:head>
 
 <Ogp
   title={post.title}
   description={post.about}
   {url}
-  image={`https://azukiazusa.dev/blog/ogp/${encodeURIComponent(post.title)}/${tagNames.join('/')}.png`}
+  image={`${variables.baseURL}/blog/ogp/${encodeURIComponent(post.title)}/${tagNames.join('/')}.png`}
 />
 
 <div class="my-12">
   <Card title={post.title} tags={post.tagsCollection.items} createdAt={post.createdAt} {contents} />
   <div class="bg-white dark:bg-gray-700 p-6 rounded-lg shadow-lg mt-2 max-w-5xl mx-auto">
     <GitHubEditButton slug={post.slug} />
-    {#if supportNavigationShare}
-      <div class="mt-4">
-        <ShareButton on:click={share} />
-      </div>
-    {:else}
-      <div class="flex mt-4">
-        <div>この記事をシェアする</div>
-        <div class="ml-4">
-          <TwitterShareButton {url} text={post.title} />
-        </div>
-        <div class="ml-4">
-          <HatenaShareButton {url} text={post.title} />
-        </div>
-      </div>
-    {/if}
+    <div class="flex mt-4">
+      <ShareButton {url} title={post.title} text={post.about}>
+        <svelte:fragment slot="fallback">
+          <div>この記事をシェアする</div>
+          <div class="ml-4">
+            <TwitterShareButton {url} text={post.title} />
+          </div>
+          <div class="ml-4">
+            <HatenaShareButton {url} text={post.title} />
+          </div>
+        </svelte:fragment>
+      </ShareButton>
+    </div>
   </div>
 </div>
 
