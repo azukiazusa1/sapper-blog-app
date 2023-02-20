03a8ca1a54dda51eca520e6af5e19ba1e5c16487
azukiazusa1 <cycleippei@gmail.com>
2023-02-20 22:07:27 +0900

    #close 189 share button

---
+++ app/src/components/ShareButton.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import ShareIcon from './Icons/Share.svelte'
+</script>
+
+<button
+  type="button"
+  class="group inline-flex items-center h-9 rounded-full text-sm font-semibold whitespace-nowrap px-3 focus:outline-none focus:ring-2 bg-gray-100 text-slate-900 hover:bg-gray-200 focus:ring-gray-500 dark:bg-gray-600 dark:text-gray-50 dark:hover:bg-gray-500 dark:focus:ring-gray-400"
+>
+  <ShareIcon className="h-6 w-6 mr-2" />
+  <span>Share</span>
+</button>
--- app/src/routes/blog/[slug]/+page.svelte
+++ app/src/routes/blog/[slug]/+page.svelte
@@ -5,11 +5,25 @@
   import PostList from '../../../components/PostList.svelte'
   import TwitterShareButton from '../../../components/TwitterShareButton.svelte'
   import HatenaShareButton from '../../../components/HatenaShareButton.svelte'
-  import GitHUbEditButton from '../../../components/GitHubEditButton.svelte'
+  import GitHubEditButton from '../../../components/GitHubEditButton.svelte'
   import type { PageData } from './$types'
   import variables from '$lib/variables'
+  import ShareButton from '../../../components/ShareButton.svelte'
 
   export let data: PageData
+
+  const share = async () => {
+    if (!navigator.share) return
+    await navigator
+      .share({
+        title: data.post.title,
+        text: data.post.about,
+        url: `${variables.baseURL}/blog/${data.post.slug}`,
+      })
+      .catch(() => {
+        // noop
+      })
+  }
 
   $: post = data.post
   $: contents = data.contents
@@ -17,6 +31,7 @@
 
   $: protocol = process.env.NODE_ENV === 'development' ? 'http' : 'https'
   $: url = `${protocol}://${$page.url.host}${$page.url.pathname}`
+  $: supportNavigationShare = 'share' in navigator
 </script>
 
 <svelte:head>
@@ -35,16 +50,22 @@
 <div class="my-12">
   <Card title={post.title} tags={post.tagsCollection.items} createdAt={post.createdAt} {contents} />
   <div class="bg-white dark:bg-gray-700 p-6 rounded-lg shadow-lg mt-2 max-w-5xl mx-auto">
-    <GitHUbEditButton slug={post.slug} />
-    <div class="flex mt-4">
-      <div>この記事をシェアする</div>
-      <div class="ml-4">
-        <TwitterShareButton {url} text={post.title} />
+    <GitHubEditButton slug={post.slug} />
+    {#if supportNavigationShare}
+      <div class="mt-4">
+        <ShareButton on:click={share} />
       </div>
-      <div class="ml-4">
-        <HatenaShareButton {url} text={post.title} />
+    {:else}
+      <div class="flex mt-4">
+        <div>この記事をシェアする</div>
+        <div class="ml-4">
+          <TwitterShareButton {url} text={post.title} />
+        </div>
+        <div class="ml-4">
+          <HatenaShareButton {url} text={post.title} />
+        </div>
       </div>
-    </div>
+    {/if}
   </div>
 </div>
 
