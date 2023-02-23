a50a5c7d831c0a2bc7c507ff559d2a3c719750ce
azukiazusa1 <cycleippei@gmail.com>
2023-02-23 12:59:59 +0900

    skip generate ogp

--- app/src/routes/blog/ogp/[title]/[...tag].png/+server.ts
+++ app/src/routes/blog/ogp/[title]/[...tag].png/+server.ts
@@ -5,7 +5,8 @@ export const prerender = true
 export const GET: RequestHandler = async ({ params }) => {
   const { title, tag = '' } = params
   const tags = tag.split('/')
-  const png = await generateOgpImage(title, tags)
+  // 本番ビルド時のみOGP画像を生成する
+  const png = process.env.VERCEL_ENV === 'production' ? await generateOgpImage(title, tags) : Buffer.from('')
 
   return new Response(png, {
     headers: {
