3267e4c333f1c626cfd55a4cf841bcb0c9d6a1b0
azukiazusa1 <59350345+azukiazusa1@users.noreply.github.com>
2023-03-01 19:51:52 +0900

    articlee max width (#238)
    * articlee max width
    
    * 画像を読み込ませない
    
    * 何故か落ちるからやめる

--- app/src/components/Card/Card.svelte
+++ app/src/components/Card/Card.svelte
@@ -11,9 +11,9 @@
   export let preview = false
 </script>
 
-<article class="max-w-5xl mx-auto">
+<article>
   <Box>
-    <div class="px-0 md:px-8">
+    <div class="px-0 md:px-4">
       <h1 class="text-2xl md:text-4xl font-bold mb-8">{title}</h1>
       <div class="flex flex-wrap items-center leading-none mt-2 mb-2">
         {#each tags as tag (tag.slug)}
