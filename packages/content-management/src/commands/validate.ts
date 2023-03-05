282c6b8deff7356cbf79980fa042c8d21177b59c
azukiazusa1 <cycleippei@gmail.com>
2023-03-05 12:08:20 +0900

    a

--- packages/content-management/src/commands/validate.ts
+++ packages/content-management/src/commands/validate.ts
@@ -1,10 +1,19 @@
 import { loadBlogPost } from '../fileOperation.js'
 import { basename } from 'path'
+import * as core from '@actions/core'
+import { getOctokit } from '@actions/github'
 
 const { ADDED_FILES, MODIFIED_FILES } = process.env
 
 console.log('Added files:', ADDED_FILES)
 console.log('Modified files:', MODIFIED_FILES)
+
+const owner = core.getInput('owner', { required: true })
+const repo = core.getInput('repo', { required: true })
+const pr_number = core.getInput('pr_number', { required: true })
+const token = core.getInput('token', { required: true })
+
+const octokit = getOctokit(token)
 
 const getFilename = (path: string) => basename(path, '.md')
 
@@ -16,7 +25,12 @@ if (ADDED_FILES) {
     const result = await loadBlogPost(filename)
     if (!result.success) {
       hasError = true
-      console.error(result.error)
+      octokit.rest.issues.createComment({
+        owner,
+        repo,
+        issue_number: Number(pr_number),
+        body: `${result.error}`,
+      })
     }
   }
   if (hasError) {
@@ -32,7 +46,12 @@ if (MODIFIED_FILES) {
     const result = await loadBlogPost(filename)
     if (!result.success) {
       hasError = true
-      console.error(result.error)
+      octokit.rest.issues.createComment({
+        owner,
+        repo,
+        issue_number: Number(pr_number),
+        body: `${result.error}`,
+      })
     }
   }
   if (hasError) {
