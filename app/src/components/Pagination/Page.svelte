e185ece9e46d6c60badfb0d485655eab187ab9d4
azukiazusa1 <59350345+azukiazusa1@users.noreply.github.com>
2023-04-08 14:58:02 +0900

    pagenation (#292)

--- app/src/components/Pagination/Page.svelte
+++ app/src/components/Pagination/Page.svelte
@@ -3,9 +3,9 @@
   export let current: boolean
   export let sm = false
 
-  const className = `w-12 ${
-    sm ? 'md:hidden flex' : 'md:flex hidden'
-  } justify-center items-center cursor-pointer leading-5 transition duration-150 ease-in rounded-full`
+  $: className = `h-12 ${sm ? 'px-4 md:hidden flex' : 'w-12 md:flex hidden'} 
+  ${current && !sm ? 'bg-gray-200 dark:bg-zinc-600' : 'hover:bg-gray-100 dark:hover:bg-zinc-700'}
+  border border-gray-200 dark:border-zinc-600 justify-center items-center cursor-pointer leading-5`
 </script>
 
 {#if href}
@@ -17,10 +17,3 @@
     <slot />
   </div>
 {/if}
-
-<style>
-  [aria-current] {
-    background-color: #df3600;
-    color: white;
-  }
-</style>
