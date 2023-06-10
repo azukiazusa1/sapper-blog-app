2e20233d9634f1df094fd66d25232cb23c9c6ce9
azukiazusa1 <59350345+azukiazusa1@users.noreply.github.com>
2023-06-10 09:36:07 +0000

    Deploying to gh-pages from @ azukiazusa1/sapper-blog-app@135caa07637265d87f827dc426d9785d55d8de38 🚀

---
+++ assets/Pagination.stories-00a84cad.js
@@ -0,0 +1,88 @@
+import{w as M,e as E}from"./index-6e68bdcb.js";import{S as F,i as G,s as J,O,P as S,w as B,d,x as c,a as k,G as w,H as x,e as C,g as N,t as b,c as z,b as v,r as ge,u as q,v as P,y as ye,z as ve,A as _e,E as A,j as K,F as I,k as L,m as Q,J as Be,p as U,B as R,C as D,D as X,K as Ee}from"./index-5a36c319.js";import"./index-3ea3853e.js";import"./index-d475d2ea.js";import"./_commonjsHelpers-87174ba5.js";import"./uniq-96991f69.js";import"./_getTag-4476a341.js";import"./index-356e4a49.js";function qe(l){let e,t;return{c(){e=O("svg"),t=O("polyline"),this.h()},l(a){e=S(a,"svg",{xmlns:!0,width:!0,height:!0,fill:!0,viewBox:!0,stroke:!0,"stroke-width":!0,"stroke-linecap":!0,"stroke-linejoin":!0,class:!0});var s=B(e);t=S(s,"polyline",{points:!0}),B(t).forEach(d),s.forEach(d),this.h()},h(){c(t,"points","15 18 9 12 15 6"),c(e,"xmlns","http://www.w3.org/2000/svg"),c(e,"width","100%"),c(e,"height","100%"),c(e,"fill","none"),c(e,"viewBox","0 0 24 24"),c(e,"stroke","currentColor"),c(e,"stroke-width","2"),c(e,"stroke-linecap","round"),c(e,"stroke-linejoin","round"),c(e,"class","feather feather-chevron-left h-6 w-6")},m(a,s){k(a,e,s),w(e,t)},p:x,i:x,o:x,d(a){a&&d(e)}}}class ke extends F{constructor(e){super(),G(this,e,null,qe,J,{})}}ke.__docgen={version:3,name:"Prev.svelte",data:[],computed:[],methods:[],components:[],description:null,keywords:[],events:[],slots:[],refs:[]};function Pe(l){let e,t;return{c(){e=O("svg"),t=O("polyline"),this.h()},l(a){e=S(a,"svg",{xmlns:!0,width:!0,height:!0,fill:!0,viewBox:!0,stroke:!0,"stroke-width":!0,"stroke-linecap":!0,"stroke-linejoin":!0,class:!0});var s=B(e);t=S(s,"polyline",{points:!0}),B(t).forEach(d),s.forEach(d),this.h()},h(){c(t,"points","9 18 15 12 9 6"),c(e,"xmlns","http://www.w3.org/2000/svg"),c(e,"width","100%"),c(e,"height","100%"),c(e,"fill","none"),c(e,"viewBox","0 0 24 24"),c(e,"stroke","currentColor"),c(e,"stroke-width","2"),c(e,"stroke-linecap","round"),c(e,"stroke-linejoin","round"),c(e,"class","feather feather-chevron-right h-6 w-6")},m(a,s){k(a,e,s),w(e,t)},p:x,i:x,o:x,d(a){a&&d(e)}}}class be extends F{constructor(e){super(),G(this,e,null,Pe,J,{})}}be.__docgen={version:3,name:"Next.svelte",data:[],computed:[],methods:[],components:[],description:null,keywords:[],events:[],slots:[],refs:[]};function Re(l){let e,t,a;const s=l[5].default,n=ge(s,l,l[4],null);return{c(){e=q("div"),n&&n.c(),this.h()},l(r){e=P(r,"DIV",{class:!0,"aria-current":!0});var i=B(e);n&&n.l(i),i.forEach(d),this.h()},h(){c(e,"class",l[2]),c(e,"aria-current",t=l[1]?"page":void 0)},m(r,i){k(r,e,i),n&&n.m(e,null),a=!0},p(r,i){n&&n.p&&(!a||i&16)&&ye(n,s,r,r[4],a?_e(s,r[4],i,null):ve(r[4]),null),(!a||i&4)&&c(e,"class",r[2]),(!a||i&2&&t!==(t=r[1]?"page":void 0))&&c(e,"aria-current",t)},i(r){a||(v(n,r),a=!0)},o(r){b(n,r),a=!1},d(r){r&&d(e),n&&n.d(r)}}}function De(l){let e,t,a;const s=l[5].default,n=ge(s,l,l[4],null);return{c(){e=q("a"),n&&n.c(),this.h()},l(r){e=P(r,"A",{href:!0,class:!0,"aria-current":!0});var i=B(e);n&&n.l(i),i.forEach(d),this.h()},h(){c(e,"href",l[0]),c(e,"class",l[2]),c(e,"aria-current",t=l[1]?"page":void 0)},m(r,i){k(r,e,i),n&&n.m(e,null),a=!0},p(r,i){n&&n.p&&(!a||i&16)&&ye(n,s,r,r[4],a?_e(s,r[4],i,null):ve(r[4]),null),(!a||i&1)&&c(e,"href",r[0]),(!a||i&4)&&c(e,"class",r[2]),(!a||i&2&&t!==(t=r[1]?"page":void 0))&&c(e,"aria-current",t)},i(r){a||(v(n,r),a=!0)},o(r){b(n,r),a=!1},d(r){r&&d(e),n&&n.d(r)}}}function xe(l){let e,t,a,s;const n=[De,Re],r=[];function i(o,u){return o[0]?0:1}return e=i(l),t=r[e]=n[e](l),{c(){t.c(),a=C()},l(o){t.l(o),a=C()},m(o,u){r[e].m(o,u),k(o,a,u),s=!0},p(o,[u]){let g=e;e=i(o),e===g?r[e].p(o,u):(N(),b(r[g],1,1,()=>{r[g]=null}),z(),t=r[e],t?t.p(o,u):(t=r[e]=n[e](o),t.c()),v(t,1),t.m(a.parentNode,a))},i(o){s||(v(t),s=!0)},o(o){b(t),s=!1},d(o){r[e].d(o),o&&d(a)}}}function Ae(l,e,t){let a,{$$slots:s={},$$scope:n}=e,{href:r=""}=e,{current:i}=e,{sm:o=!1}=e;return l.$$set=u=>{"href"in u&&t(0,r=u.href),"current"in u&&t(1,i=u.current),"sm"in u&&t(3,o=u.sm),"$$scope"in u&&t(4,n=u.$$scope)},l.$$.update=()=>{l.$$.dirty&10&&t(2,a=`h-12 ${o?"px-4 md:hidden flex":"w-12 md:flex hidden"} 
+  ${i&&!o?"bg-gray-200 dark:bg-zinc-600":"hover:bg-gray-100 dark:hover:bg-zinc-700"}
+  border border-gray-200 dark:border-zinc-600 justify-center items-center cursor-pointer leading-5`)},[r,i,a,o,n,s]}class Y extends F{constructor(e){super(),G(this,e,Ae,xe,J,{href:0,current:1,sm:3})}}Y.__docgen={version:3,name:"Page.svelte",data:[{visibility:"public",description:null,keywords:[],name:"href",kind:"let",static:!1,readonly:!1,type:{kind:"type",text:"string",type:"string"},defaultValue:""},{visibility:"public",description:null,keywords:[],name:"current",kind:"let",static:!1,readonly:!1,type:{kind:"type",text:"any",type:"any"}},{visibility:"public",description:null,keywords:[],name:"sm",kind:"let",static:!1,readonly:!1,type:{kind:"type",text:"boolean",type:"boolean"},defaultValue:!1}],computed:[],methods:[],components:[],description:null,keywords:[],events:[],slots:[{keywords:[],visibility:"public",description:"",name:"default"}],refs:[]};function ee(l,e,t){const a=l.slice();return a[9]=e[t],a[11]=t,a}function te(l){let e,t,a,s,n,r,i;return n=new ke({}),{c(){e=q("a"),t=q("div"),a=R("前のページ"),s=A(),K(n.$$.fragment),this.h()},l(o){e=P(o,"A",{class:!0,href:!0,"data-sveltekit-preload-data":!0});var u=B(e);t=P(u,"DIV",{class:!0});var g=B(t);a=D(g,"前のページ"),g.forEach(d),s=I(u),L(n.$$.fragment,u),u.forEach(d),this.h()},h(){c(t,"class","sr-only"),c(e,"class","flex h-12 w-12 items-center justify-center rounded-l-lg border border-gray-200 dark:border-zinc-600"),c(e,"href",r=`${l[1]}${l[4]}`),c(e,"data-sveltekit-preload-data","")},m(o,u){k(o,e,u),w(e,t),w(t,a),w(e,s),Q(n,e,null),i=!0},p(o,u){(!i||u&18&&r!==(r=`${o[1]}${o[4]}`))&&c(e,"href",r)},i(o){i||(v(n.$$.fragment,o),i=!0)},o(o){b(n.$$.fragment,o),i=!1},d(o){o&&d(e),U(n)}}}function Ie(l){let e=l[11]+1+"",t;return{c(){t=R(e)},l(a){t=D(a,e)},m(a,s){k(a,t,s)},p(a,s){s&4&&e!==(e=a[11]+1+"")&&X(t,e)},d(a){a&&d(t)}}}function ne(l,e){let t,a,s;return a=new Y({props:{href:`${e[1]}${e[11]+1}`,current:e[0]===e[11]+1,$$slots:{default:[Ie]},$$scope:{ctx:e}}}),{key:l,first:null,c(){t=C(),K(a.$$.fragment),this.h()},l(n){t=C(),L(a.$$.fragment,n),this.h()},h(){this.first=t},m(n,r){k(n,t,r),Q(a,n,r),s=!0},p(n,r){e=n;const i={};r&6&&(i.href=`${e[1]}${e[11]+1}`),r&5&&(i.current=e[0]===e[11]+1),r&4100&&(i.$$scope={dirty:r,ctx:e}),a.$set(i)},i(n){s||(v(a.$$.fragment,n),s=!0)},o(n){b(a.$$.fragment,n),s=!1},d(n){n&&d(t),U(a,n)}}}function $e(l){let e,t,a,s;return{c(){e=R("Page "),t=R(l[0]),a=R(" of "),s=R(l[2])},l(n){e=D(n,"Page "),t=D(n,l[0]),a=D(n," of "),s=D(n,l[2])},m(n,r){k(n,e,r),k(n,t,r),k(n,a,r),k(n,s,r)},p(n,r){r&1&&X(t,n[0]),r&4&&X(s,n[2])},d(n){n&&d(e),n&&d(t),n&&d(a),n&&d(s)}}}function ae(l){let e,t,a,s,n,r,i;return n=new be({}),{c(){e=q("a"),t=q("div"),a=R("次のページ"),s=A(),K(n.$$.fragment),this.h()},l(o){e=P(o,"A",{class:!0,href:!0,"data-sveltekit-preload-data":!0});var u=B(e);t=P(u,"DIV",{class:!0});var g=B(t);a=D(g,"次のページ"),g.forEach(d),s=I(u),L(n.$$.fragment,u),u.forEach(d),this.h()},h(){c(t,"class","sr-only"),c(e,"class","ml-1 flex h-12 w-12 items-center justify-center rounded-r-lg border border-gray-200 dark:border-zinc-600"),c(e,"href",r=`${l[1]}${l[3]}`),c(e,"data-sveltekit-preload-data","")},m(o,u){k(o,e,u),w(e,t),w(t,a),w(e,s),Q(n,e,null),i=!0},p(o,u){(!i||u&10&&r!==(r=`${o[1]}${o[3]}`))&&c(e,"href",r)},i(o){i||(v(n.$$.fragment,o),i=!0)},o(o){b(n.$$.fragment,o),i=!1},d(o){o&&d(e),U(n)}}}function He(l){let e,t,a,s,n=[],r=new Map,i,o,u,g,m=l[6]&&te(l),_=Array(l[2]);const Z=f=>f[11];for(let f=0;f<_.length;f+=1){let p=ee(l,_,f),y=Z(p);r.set(y,n[f]=ne(y,p))}o=new Y({props:{current:!0,sm:!0,$$slots:{default:[$e]},$$scope:{ctx:l}}});let h=l[5]&&ae(l);return{c(){e=q("nav"),t=q("div"),m&&m.c(),a=A(),s=q("div");for(let f=0;f<n.length;f+=1)n[f].c();i=A(),K(o.$$.fragment),u=A(),h&&h.c(),this.h()},l(f){e=P(f,"NAV",{class:!0,"aria-label":!0});var p=B(e);t=P(p,"DIV",{class:!0});var y=B(t);m&&m.l(y),a=I(y),s=P(y,"DIV",{class:!0});var $=B(s);for(let W=0;W<n.length;W+=1)n[W].l($);i=I($),L(o.$$.fragment,$),$.forEach(d),u=I(y),h&&h.l(y),y.forEach(d),p.forEach(d),this.h()},h(){c(s,"class","flex -space-x-px rounded-full font-medium"),c(t,"class","flex -space-x-px"),c(e,"class","my-12 flex flex-col items-center"),c(e,"aria-label","ページネーション")},m(f,p){k(f,e,p),w(e,t),m&&m.m(t,null),w(t,a),w(t,s);for(let y=0;y<n.length;y+=1)n[y].m(s,null);w(s,i),Q(o,s,null),w(t,u),h&&h.m(t,null),g=!0},p(f,[p]){f[6]?m?(m.p(f,p),p&64&&v(m,1)):(m=te(f),m.c(),v(m,1),m.m(t,a)):m&&(N(),b(m,1,1,()=>{m=null}),z()),p&7&&(_=Array(f[2]),N(),n=Be(n,p,Z,1,f,_,r,s,Ee,ne,i,ee),z());const y={};p&4101&&(y.$$scope={dirty:p,ctx:f}),o.$set(y),f[5]?h?(h.p(f,p),p&32&&v(h,1)):(h=ae(f),h.c(),v(h,1),h.m(t,null)):h&&(N(),b(h,1,1,()=>{h=null}),z())},i(f){if(!g){v(m);for(let p=0;p<_.length;p+=1)v(n[p]);v(o.$$.fragment,f),v(h),g=!0}},o(f){b(m);for(let p=0;p<n.length;p+=1)b(n[p]);b(o.$$.fragment,f),b(h),g=!1},d(f){f&&d(e),m&&m.d();for(let p=0;p<n.length;p+=1)n[p].d();U(o),h&&h.d()}}}function Te(l,e,t){let a,s,n,r,i,{page:o=1}=e,{total:u}=e,{limit:g}=e,{href:m="/blog/page/"}=e;return l.$$set=_=>{"page"in _&&t(0,o=_.page),"total"in _&&t(7,u=_.total),"limit"in _&&t(8,g=_.limit),"href"in _&&t(1,m=_.href)},l.$$.update=()=>{l.$$.dirty&384&&t(2,a=Math.ceil(u/g)),l.$$.dirty&1&&t(6,s=o!==1),l.$$.dirty&5&&t(5,n=o!==a&&a!==0),l.$$.dirty&1&&t(4,r=o-1),l.$$.dirty&1&&t(3,i=o+1)},[o,m,a,i,r,n,s,u,g]}class we extends F{constructor(e){super(),G(this,e,Te,He,J,{page:0,total:7,limit:8,href:1})}}we.__docgen={version:3,name:"Pagination.svelte",data:[{visibility:"public",description:null,keywords:[],name:"page",kind:"let",static:!1,readonly:!1,type:{kind:"type",text:"number",type:"number"},defaultValue:1},{visibility:"public",description:null,keywords:[],name:"total",kind:"let",static:!1,readonly:!1,type:{kind:"type",text:"any",type:"any"}},{visibility:"public",description:null,keywords:[],name:"limit",kind:"let",static:!1,readonly:!1,type:{kind:"type",text:"any",type:"any"}},{visibility:"public",description:null,keywords:[],name:"href",kind:"let",static:!1,readonly:!1,type:{kind:"type",text:"string",type:"string"},defaultValue:"/blog/page/"}],computed:[],methods:[],components:[],description:null,keywords:[],events:[],slots:[],refs:[]};const Fe={title:"Pagination",component:we,tags:["autodocs"],argTypes:{page:{control:{type:"number"},description:"現在のページ",default:1},total:{control:{type:"number"},description:"総アイテム数"},limit:{control:{type:"number"},description:"1ページあたりのアイテム数"},href:{control:{type:"text"},description:"ページネーションのリンク先",default:"/blog/page/"}}},H={args:{page:1,total:100,limit:10},play:async({canvasElement:l})=>{const e=M(l),t=e.queryByRole("link",{name:"前のページ"}),a=e.queryByRole("link",{name:"次のページ"}),s=e.queryByRole("link",{name:"1"});E(t).not.toBeInTheDocument(),E(a).toHaveAttribute("href","/blog/page/2"),E(s).toHaveAttribute("aria-current","page")}},T={args:{page:2,total:100,limit:10},play:async({canvasElement:l})=>{const e=M(l),t=e.queryByRole("link",{name:"2"}),a=e.queryByRole("link",{name:"前のページ"}),s=e.queryByRole("link",{name:"次のページ"});E(a).toHaveAttribute("href","/blog/page/1"),E(s).toHaveAttribute("href","/blog/page/3"),E(t).toHaveAttribute("aria-current","page")}},V={args:{page:10,total:100,limit:10},play:async({canvasElement:l})=>{const e=M(l),t=e.queryByRole("link",{name:"前のページ"}),a=e.queryByRole("link",{name:"次のページ"});E(t).toBeInTheDocument(),E(a).not.toBeInTheDocument()}},j={args:{page:1,total:10,limit:10},play:async({canvasElement:l})=>{const e=M(l),t=e.queryByRole("link",{name:"前のページ"}),a=e.queryByRole("link",{name:"次のページ"});E(t).not.toBeInTheDocument(),E(a).not.toBeInTheDocument()}};var re,le,se;H.parameters={...H.parameters,docs:{...(re=H.parameters)==null?void 0:re.docs,source:{originalSource:`{
+  args: {
+    page: 1,
+    total: 100,
+    limit: 10
+  },
+  play: async ({
+    canvasElement
+  }) => {
+    const canvas = within(canvasElement);
+    const prev = canvas.queryByRole("link", {
+      name: "前のページ"
+    });
+    const next = canvas.queryByRole("link", {
+      name: "次のページ"
+    });
+    const page1 = canvas.queryByRole("link", {
+      name: "1"
+    });
+    expect(prev).not.toBeInTheDocument();
+    expect(next).toHaveAttribute("href", "/blog/page/2");
+    expect(page1).toHaveAttribute("aria-current", "page");
+  }
+}`,...(se=(le=H.parameters)==null?void 0:le.docs)==null?void 0:se.source}}};var oe,ie,ce;T.parameters={...T.parameters,docs:{...(oe=T.parameters)==null?void 0:oe.docs,source:{originalSource:`{
+  args: {
+    page: 2,
+    total: 100,
+    limit: 10
+  },
+  play: async ({
+    canvasElement
+  }) => {
+    const canvas = within(canvasElement);
+    const page2 = canvas.queryByRole("link", {
+      name: "2"
+    });
+    const prev = canvas.queryByRole("link", {
+      name: "前のページ"
+    });
+    const next = canvas.queryByRole("link", {
+      name: "次のページ"
+    });
+    expect(prev).toHaveAttribute("href", "/blog/page/1");
+    expect(next).toHaveAttribute("href", "/blog/page/3");
+    expect(page2).toHaveAttribute("aria-current", "page");
+  }
+}`,...(ce=(ie=T.parameters)==null?void 0:ie.docs)==null?void 0:ce.source}}};var ue,fe,pe;V.parameters={...V.parameters,docs:{...(ue=V.parameters)==null?void 0:ue.docs,source:{originalSource:`{
+  args: {
+    page: 10,
+    total: 100,
+    limit: 10
+  },
+  play: async ({
+    canvasElement
+  }) => {
+    const canvas = within(canvasElement);
+    const prev = canvas.queryByRole("link", {
+      name: "前のページ"
+    });
+    const next = canvas.queryByRole("link", {
+      name: "次のページ"
+    });
+    expect(prev).toBeInTheDocument();
+    expect(next).not.toBeInTheDocument();
+  }
+}`,...(pe=(fe=V.parameters)==null?void 0:fe.docs)==null?void 0:pe.source}}};var de,me,he;j.parameters={...j.parameters,docs:{...(de=j.parameters)==null?void 0:de.docs,source:{originalSource:`{
+  args: {
+    page: 1,
+    total: 10,
+    limit: 10
+  },
+  play: async ({
+    canvasElement
+  }) => {
+    const canvas = within(canvasElement);
+    const prev = canvas.queryByRole("link", {
+      name: "前のページ"
+    });
+    const next = canvas.queryByRole("link", {
+      name: "次のページ"
+    });
+    expect(prev).not.toBeInTheDocument();
+    expect(next).not.toBeInTheDocument();
+  }
+}`,...(he=(me=j.parameters)==null?void 0:me.docs)==null?void 0:he.source}}};const Ge=["Default","Page2","NoMorePage","OnlyOnePage"];export{H as Default,V as NoMorePage,j as OnlyOnePage,T as Page2,Ge as __namedExportsOrder,Fe as default};
+//# sourceMappingURL=Pagination.stories-00a84cad.js.map
