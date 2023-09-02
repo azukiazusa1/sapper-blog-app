979c4e9dfc4497ead9d40c014c717eb2e15d9555
azukiazusa1 <cycleippei@gmail.com>
2023-09-02 17:38:27 +0900

    ogp を prerender しない

--- app/src/repositories/AnalyticsData/MockAnalyticsDataRepository.ts
+++ app/src/repositories/AnalyticsData/MockAnalyticsDataRepository.ts
@@ -5,21 +5,7 @@ export class MockAnalyticsDataRepository
 {
   getPopularPosts(): Promise<PopularPost[]> {
     return Promise.resolve([
-      {
-        title: "はじめての GraphQL",
-        path: "/blog/what-is-graphql",
-        views: 100,
-      },
-      {
-        title: "JavaScriptライブラリSvelteとは",
-        path: "/blog/what-is-svelte",
-        views: 50,
-      },
-      {
-        title: "Go 言語のパッケージ管理ツール Go Modules",
-        path: "/blog/go-modules-go-1-11-go-gopath-go-usdgopath-src",
-        views: 10,
-      },
+      
     ]);
   }
 }
