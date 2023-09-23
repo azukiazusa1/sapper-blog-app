b7685a6d90f995f7ccc599b80bbd14fd76cc81cc
azukiazusa1 <cycleippei@gmail.com>
2023-09-23 09:54:56 +0900

    format

--- app/src/components/Card/Card.svelte
+++ app/src/components/Card/Card.svelte
@@ -17,8 +17,8 @@
   onMount(() => {
     // コードブロックに、コピー用のボタンを追加
     const codeBlocks = document.querySelectorAll("pre");
-    const copyIcon =  `<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 448 512" class="w-4 h-4"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M208 0H332.1c12.7 0 24.9 5.1 33.9 14.1l67.9 67.9c9 9 14.1 21.2 14.1 33.9V336c0 26.5-21.5 48-48 48H208c-26.5 0-48-21.5-48-48V48c0-26.5 21.5-48 48-48zM48 128h80v64H64V448H256V416h64v48c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V176c0-26.5 21.5-48 48-48z"/></svg>`;
-    const checkIcon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4"><path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" /></svg>`
+    const copyIcon = `<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 448 512" class="w-4 h-4"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M208 0H332.1c12.7 0 24.9 5.1 33.9 14.1l67.9 67.9c9 9 14.1 21.2 14.1 33.9V336c0 26.5-21.5 48-48 48H208c-26.5 0-48-21.5-48-48V48c0-26.5 21.5-48 48-48zM48 128h80v64H64V448H256V416h64v48c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V176c0-26.5 21.5-48 48-48z"/></svg>`;
+    const checkIcon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4"><path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" /></svg>`;
     codeBlocks.forEach((block) => {
       const button = document.createElement("button");
       button.classList.add(
@@ -65,7 +65,7 @@
         "opacity-0",
         "pointer-events-none",
       );
-      popup.textContent = "Copied!"
+      popup.textContent = "Copied!";
       block.appendChild(popup);
       button.addEventListener("click", () => {
         navigator.clipboard.writeText(block.textContent);
@@ -80,18 +80,18 @@
         popup.classList.remove("opacity-0");
 
         // 1秒後にポップアップを消す
-       setTimeout(() => {
+        setTimeout(() => {
           popup.classList.add("opacity-0");
           button.innerHTML = copyIcon;
           button.classList.remove("text-green-500");
           button.classList.add("text-gray-200");
           button.classList.add("hover:text-gray-500");
           button.classList.add("focus:text-gray-500");
-        }, 1000); 
+        }, 1000);
       });
       block.appendChild(button);
     });
-  })
+  });
 </script>
 
 <article>
