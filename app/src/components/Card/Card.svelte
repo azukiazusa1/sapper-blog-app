e26ce0bf6210eac14d703507b092b226137f4594
azukiazusa1 <cycleippei@gmail.com>
2023-09-23 09:52:28 +0900

    copy

--- app/src/components/Card/Card.svelte
+++ app/src/components/Card/Card.svelte
@@ -17,58 +17,77 @@
   onMount(() => {
     // コードブロックに、コピー用のボタンを追加
     const codeBlocks = document.querySelectorAll("pre");
+    const copyIcon =  `<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 448 512" class="w-4 h-4"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M208 0H332.1c12.7 0 24.9 5.1 33.9 14.1l67.9 67.9c9 9 14.1 21.2 14.1 33.9V336c0 26.5-21.5 48-48 48H208c-26.5 0-48-21.5-48-48V48c0-26.5 21.5-48 48-48zM48 128h80v64H64V448H256V416h64v48c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V176c0-26.5 21.5-48 48-48z"/></svg>`;
+    const checkIcon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4"><path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" /></svg>`
     codeBlocks.forEach((block) => {
       const button = document.createElement("button");
       button.classList.add(
         "absolute",
         "right-2",
         "top-2",
-        "p-1",
+        "rounded-md",
+        "text-gray-200",
+        "opacity-50",
+        "p-2",
+        "ring-2",
+        "ring-gray-500",
+        "ring-opacity-50",
+        "text-outline-none",
+        "hover:text-gray-500",
+        "hover:bg-gray-500",
+        "hover:opacity-100",
+        "hover:ring-2",
+        "hover:ring-gray-500",
+        "focus:text-gray-500",
+        "focus:ring-2",
+        "focus:ring-gray-500",
+        "focus:opacity-100",
+      );
+      // エディタと同じ色
+      button.style.backgroundColor = "#212121";
+      button.innerHTML = copyIcon;
+      button.ariaLabel = "コピー";
+      const popup = document.createElement("div");
+      popup.classList.add(
+        "absolute",
+        "top-2",
+        "right-14",
+        "px-2",
+        "py-1",
         "rounded-md",
         "bg-gray-100",
         "dark:bg-zinc-500",
         "text-gray-500",
         "dark:text-gray-300",
-        "hover:bg-gray-200",
-        "dark:hover:bg-zinc-400",
-        "focus:outline-none",
-        "focus:ring-2",
-        "focus:ring-gray-200",
-        "dark:focus:ring-zinc-400",
-        "focus:ring-opacity-50",
-        "dark:focus:ring-opacity-50",
+        "text-sm",
+        "transition-opacity",
+        "duration-300",
+        "opacity-0",
+        "pointer-events-none",
       );
-      button.textContent = `<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M208 0H332.1c12.7 0 24.9 5.1 33.9 14.1l67.9 67.9c9 9 14.1 21.2 14.1 33.9V336c0 26.5-21.5 48-48 48H208c-26.5 0-48-21.5-48-48V48c0-26.5 21.5-48 48-48zM48 128h80v64H64V448H256V416h64v48c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V176c0-26.5 21.5-48 48-48z"/></svg>`;
-      button.ariaLabel = "コピー";
+      popup.textContent = "Copied!"
+      block.appendChild(popup);
       button.addEventListener("click", () => {
         navigator.clipboard.writeText(block.textContent);
-        // クリックしたらポップアップを表示
-        const popup = document.createElement("div");
-        popup.classList.add(
-          "absolute",
-          "top-2",
-          "right-2",
-          "p-2",
-          "rounded-md",
-          "bg-gray-100",
-          "dark:bg-zinc-500",
-          "text-gray-500",
-          "dark:text-gray-300",
-          "text-sm",
-          "transition-opacity",
-          "duration-300",
-          "opacity-0",
-          "pointer-events-none",
-        );
-        block.appendChild(popup);
+
+        button.innerHTML = checkIcon;
+        button.classList.remove("text-gray-200");
+        button.classList.remove("hover:text-gray-500");
+        button.classList.remove("focus:text-gray-500");
+        button.classList.add("text-green-500");
+
+        // ポップアップを表示
+        popup.classList.remove("opacity-0");
 
         // 1秒後にポップアップを消す
-        setTimeout(() => {
+       setTimeout(() => {
           popup.classList.add("opacity-0");
-          setTimeout(() => {
-            block.removeChild(popup);
-          }, 300);
-        }, 1000);
+          button.innerHTML = copyIcon;
+          button.classList.remove("text-green-500");
+          button.classList.add("text-gray-200");
+          button.classList.add("hover:text-gray-500");
+          button.classList.add("focus:text-gray-500");
+        }, 1000); 
       });
       block.appendChild(button);
     });
