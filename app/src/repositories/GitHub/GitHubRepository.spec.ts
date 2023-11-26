98bb23289ba3bd0615e71d722b1e7d2b133c58c7
azukiazusa1 <cycleippei@gmail.com>
2023-11-26 16:51:22 +0900

    Update dependencies in package.json files

--- app/src/repositories/GitHub/GitHubRepository.spec.ts
+++ app/src/repositories/GitHub/GitHubRepository.spec.ts
@@ -1,5 +1,5 @@
 import { describe, test, expect, vi } from "vitest";
-import { rest } from "msw";
+import { HttpResponse, http } from "msw";
 import { createDummyGitHubCommit } from "../../test-utils/createDummyGitHub";
 import type { Contributor } from "./types";
 import { GitHubRepository } from "./GitHubRepository";
@@ -10,25 +10,23 @@ describe("GitHubRepository", () => {
   test("commit から contributor の一覧を取得する", async () => {
     const fn = vi.fn();
     server.use(
-      rest.get(
+      http.get(
         "https://api.github.com/repos/azukiazusa1/sapper-blog-app/commits",
-        (req, res, ctx) => {
-          fn(req.url.searchParams.get("path"));
-          return res(
-            ctx.status(200),
-            ctx.json([
-              createDummyGitHubCommit({
-                username: "azukiazusa1",
-                avatar: "avatar1",
-                url: "url1",
-              }),
-              createDummyGitHubCommit({
-                username: "azukiazusa2",
-                avatar: "avatar2",
-                url: "url2",
-              }),
-            ]),
-          );
+        ({ request }) => {
+          const url = new URL(request.url);
+          fn(url.searchParams.get("path"));
+          return HttpResponse.json([
+            createDummyGitHubCommit({
+              username: "azukiazusa1",
+              avatar: "avatar1",
+              url: "url1",
+            }),
+            createDummyGitHubCommit({
+              username: "azukiazusa2",
+              avatar: "avatar2",
+              url: "url2",
+            }),
+          ]);
         },
       ),
     );
@@ -50,39 +48,36 @@ describe("GitHubRepository", () => {
 
   test("bot の commit は除外する", async () => {
     server.use(
-      rest.get(
+      http.get(
         "https://api.github.com/repos/azukiazusa1/sapper-blog-app/commits",
-        (req, res, ctx) => {
-          return res(
-            ctx.status(200),
-            ctx.json([
-              createDummyGitHubCommit({
-                username: "azukiazusa1",
-                avatar: "avatar1",
-                url: "url1",
-              }),
-              createDummyGitHubCommit({
-                username: "github-actions[bot]",
-                avatar: "avatar1",
-                url: "url1",
-              }),
-              createDummyGitHubCommit({
-                username: "actions-user",
-                avatar: "avatar2",
-                url: "url2",
-              }),
-              createDummyGitHubCommit({
-                username: "dependabot[bot]",
-                avatar: "avatar3",
-                url: "url3",
-              }),
-              createDummyGitHubCommit({
-                username: "renovate[bot]",
-                avatar: "avatar4",
-                url: "url4",
-              }),
-            ]),
-          );
+        () => {
+          return HttpResponse.json([
+            createDummyGitHubCommit({
+              username: "azukiazusa1",
+              avatar: "avatar1",
+              url: "url1",
+            }),
+            createDummyGitHubCommit({
+              username: "github-actions[bot]",
+              avatar: "avatar1",
+              url: "url1",
+            }),
+            createDummyGitHubCommit({
+              username: "actions-user",
+              avatar: "avatar2",
+              url: "url2",
+            }),
+            createDummyGitHubCommit({
+              username: "dependabot[bot]",
+              avatar: "avatar3",
+              url: "url3",
+            }),
+            createDummyGitHubCommit({
+              username: "renovate[bot]",
+              avatar: "avatar4",
+              url: "url4",
+            }),
+          ]);
         },
       ),
     );
@@ -98,10 +93,10 @@ describe("GitHubRepository", () => {
 
   test("commit が存在しない場合は自分を返す", async () => {
     server.use(
-      rest.get(
+      http.get(
         "https://api.github.com/repos/azukiazusa1/sapper-blog-app/commits",
-        (req, res, ctx) => {
-          return res(ctx.status(200), ctx.json([]));
+        () => {
+          return HttpResponse.json([]);
         },
       ),
     );
@@ -117,39 +112,36 @@ describe("GitHubRepository", () => {
 
   test("重複した contributor は除外する", async () => {
     server.use(
-      rest.get(
+      http.get(
         "https://api.github.com/repos/azukiazusa1/sapper-blog-app/commits",
-        (req, res, ctx) => {
-          return res(
-            ctx.status(200),
-            ctx.json([
-              createDummyGitHubCommit({
-                username: "azukiazusa1",
-                avatar: "avatar1",
-                url: "url1",
-              }),
-              createDummyGitHubCommit({
-                username: "azukiazusa1",
-                avatar: "avatar1",
-                url: "url1",
-              }),
-              createDummyGitHubCommit({
-                username: "azukiazusa2",
-                avatar: "avatar2",
-                url: "url2",
-              }),
-              createDummyGitHubCommit({
-                username: "azukiazusa1",
-                avatar: "avatar1",
-                url: "url1",
-              }),
-              createDummyGitHubCommit({
-                username: "azukiazusa3",
-                avatar: "avatar",
-                url: "url3",
-              }),
-            ]),
-          );
+        () => {
+          return HttpResponse.json([
+            createDummyGitHubCommit({
+              username: "azukiazusa1",
+              avatar: "avatar1",
+              url: "url1",
+            }),
+            createDummyGitHubCommit({
+              username: "azukiazusa1",
+              avatar: "avatar1",
+              url: "url1",
+            }),
+            createDummyGitHubCommit({
+              username: "azukiazusa2",
+              avatar: "avatar2",
+              url: "url2",
+            }),
+            createDummyGitHubCommit({
+              username: "azukiazusa1",
+              avatar: "avatar1",
+              url: "url1",
+            }),
+            createDummyGitHubCommit({
+              username: "azukiazusa3",
+              avatar: "avatar",
+              url: "url3",
+            }),
+          ]);
         },
       ),
     );
