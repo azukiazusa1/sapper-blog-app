5f4eb2d98716b1994245917be394599b7fe0a618
azukiazusa1 <59350345+azukiazusa1@users.noreply.github.com>
2024-01-01 19:20:30 +0900

    use bits-ui (#814)

--- app/src/components/Header/ToggleDarkMode.svelte
+++ app/src/components/Header/ToggleDarkMode.svelte
@@ -1,17 +1,22 @@
 <script lang="ts">
   import { onMount } from "svelte";
   import { type Theme, getTheme, changeTheme } from "../../utils/darkTheme";
-  import {
-    Listbox,
-    ListboxButton,
-    ListboxOptions,
-    ListboxOption,
-  } from "@rgossiaux/svelte-headlessui";
+  import { DropdownMenu } from "bits-ui";
   import Moon from "../Icons/Moon.svelte";
   import Sun from "../Icons/Sun.svelte";
   import System from "../Icons/system.svelte";
-  let theme: Theme = "system";
+  import { fly } from "svelte/transition";
+  import Check from "../Icons/Check.svelte";
+  let theme: Theme | undefined = undefined;
+
+  /**
+   * ポップアップを右側に表示するかどうか
+   * @default false
+   */
   export let right = false;
+
+  // bits-ui の side の型定義がおかしいので、any で回避
+  const side: any = right ? "bottom-end" : "bottom-start";
 
   onMount(() => {
     theme = getTheme();
@@ -20,49 +25,52 @@
     { value: "system", label: "System" },
     { value: "light", label: "Light" },
     { value: "dark", label: "Dark" },
-  ] as const satisfies readonly {
+  ] as const satisfies {
     value: Theme;
     label: string;
   }[];
 
-  const handleChange = (e: CustomEvent<Theme>) => {
-    theme = e.detail;
-    changeTheme(theme);
+  const handleChange = (t: Theme) => {
+    theme = t;
+    changeTheme(t);
   };
 </script>
 
-<Listbox on:change={handleChange} value={theme}>
-  <ListboxButton
+<!-- ドロワーで表示したときおかしくなるから portal を null にしている -->
+<DropdownMenu.Root portal={null}>
+  <DropdownMenu.Trigger
     class="flex items-center rounded-lg border border-gray-300 p-2 dark:border-zinc-600"
     aria-label="カラーテーマを選択する"
   >
     <Moon className="h-6 w-6 hidden dark:block" />
     <Sun className="h-6 w-6 block dark:hidden" />
-  </ListboxButton>
-  <ListboxOptions
-    class={`absolute z-20 w-24 overflow-hidden overflow-y-scroll rounded-bl-md rounded-br-md border-2 border-t-0 border-gray-200 bg-white shadow-lg dark:border-zinc-700 dark:bg-zinc-700 md:w-28 ${
-      right ? "right-2" : ""
-    }`}
+  </DropdownMenu.Trigger>
+  <DropdownMenu.Content
+    class="rounded-bl-md rounded-br-md border-2 border-t-0 border-gray-200 bg-white shadow-lg dark:border-zinc-600 dark:bg-zinc-700"
+    transition={fly}
+    {side}
   >
-    {#each items as item}
-      <ListboxOption
-        value={item.value}
-        class={({ active }) =>
-          `-mx-2 flex transform cursor-pointer items-center border-b px-4 py-3 transition-colors duration-200 last:border-0 hover:bg-gray-100 dark:border-zinc-700 dark:hover:bg-zinc-500 ${
-            active ? "bg-gray-100 dark:bg-zinc-500" : ""
-          }`}
-      >
-        {#if item.value === "system"}
-          <System className="h-5 w-5" />
-        {:else if item.value === "light"}
-          <Sun className="h-5 w-5" />
-        {:else if item.value === "dark"}
-          <Moon className="h-5 w-5" />
-        {/if}
-        <span class="ml-2 text-sm">
-          {item.label}
-        </span>
-      </ListboxOption>
-    {/each}
-  </ListboxOptions>
-</Listbox>
+    <DropdownMenu.RadioGroup value={theme} onValueChange={handleChange}>
+      {#each items as item}
+        <DropdownMenu.RadioItem
+          value={item.value}
+          class={`z-50 flex w-32 transform cursor-pointer items-center px-2 py-3 transition-colors duration-200 hover:bg-gray-100 data-[highlighted]:bg-gray-100  dark:border-zinc-600 dark:hover:bg-zinc-500 dark:data-[highlighted]:bg-zinc-500`}
+        >
+          {#if item.value === "system"}
+            <System className="h-5 w-5" />
+          {:else if item.value === "light"}
+            <Sun className="h-5 w-5" />
+          {:else if item.value === "dark"}
+            <Moon className="h-5 w-5" />
+          {/if}
+          <span class="ml-2 text-sm">
+            {item.label}
+          </span>
+          <DropdownMenu.RadioIndicator class="ml-auto">
+            <Check className="h-4 w-4 text-indigo-600 dark:text-indigo-400" />
+          </DropdownMenu.RadioIndicator>
+        </DropdownMenu.RadioItem>
+      {/each}
+    </DropdownMenu.RadioGroup>
+  </DropdownMenu.Content>
+</DropdownMenu.Root>
