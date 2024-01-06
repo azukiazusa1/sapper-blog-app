a196e69b24a5424e11f65af2d5aebadd0b40338b
azukiazusa1 <cycleippei@gmail.com>
2024-01-06 12:49:56 +0900

    fix format

--- app/src/components/SearchDialog/SearchBar.svelte
+++ app/src/components/SearchDialog/SearchBar.svelte
@@ -2,8 +2,10 @@
   import SearchIcon from "../Icons/Search.svelte";
 </script>
 
-<div class="flex w-full rounded-lg border-2 border-gray-300 dark:border-zinc-700 dark:bg-zinc-900">
-  <span class="flex justify-end items-center rounded-r-lg p-2 ">
+<div
+  class="flex w-full rounded-lg border-2 border-gray-300 dark:border-zinc-700 dark:bg-zinc-900"
+>
+  <span class="flex items-center justify-end rounded-r-lg p-2">
     <SearchIcon className="text-gray-600 dark:text-gray-50 h-4 w-4" />
   </span>
   <div
