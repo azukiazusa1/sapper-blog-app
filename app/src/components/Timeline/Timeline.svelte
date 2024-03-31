3ed97b05b49dd03d6164ec66df29d38d73f1adb4
azukiazusa1 <cycleippei@gmail.com>
2024-03-31 17:55:52 +0900

    format

--- app/src/components/Timeline/Timeline.svelte
+++ app/src/components/Timeline/Timeline.svelte
@@ -18,12 +18,30 @@
     {eventTitle}
   </h3>
   <Time date={eventDate} />
-  <p class="mt-2 text-lg font-bold text-indigo-700 dark:text-indigo-400 underline flex items-center gap-2">
-    <a class="no-underline hover:underline" href={presentationLink} target="_blank" rel="noopener noreferrer">
+  <p
+    class="mt-2 flex items-center gap-2 text-lg font-bold text-indigo-700 underline dark:text-indigo-400"
+  >
+    <a
+      class="no-underline hover:underline"
+      href={presentationLink}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
       {presentationTitle}
     </a>
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-      <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      class="h-6 w-6"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+      />
     </svg>
   </p>
   <p class="my-2 text-sm font-normal text-gray-500 dark:text-gray-400">
