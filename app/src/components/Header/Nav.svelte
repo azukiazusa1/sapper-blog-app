9700d1ea5a4bf5a3084dd34e82a1a2c1cf6fc1b5
azukiazusa1 <cycleippei@gmail.com>
2024-04-26 09:04:08 +0900

    navigation hover

--- app/src/components/Header/Nav.svelte
+++ app/src/components/Header/Nav.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
   import { isMatchPath } from "$lib/utils";
+    import clsx from "clsx";
 
   export let segment: string;
   export let routes: string[];
 </script>
 
 <nav>
-  <ul class="flex space-x-4">
+  <ul class="flex space-x-4 items-center">
     {#each routes as route}
       <li>
         <a
           aria-current={isMatchPath(route, segment) ? "page" : undefined}
           href={route}
-          class="px-3 capitalize hover:opacity-75"
-        >
+          class="px-4 py-2 capitalize hover:bg-gray-200 dark:hover:bg-zinc-700 transition-colors duration-300 ease-in-out">
           {route.slice(1)}
         </a>
       </li>
@@ -30,10 +30,10 @@
   [aria-current]::after {
     position: absolute;
     content: "";
-    width: calc(100% - 1.5rem);
+    width: calc(100% - 2rem);
     height: 2px;
     background-color: #df3600;
     display: block;
-    bottom: -8px;
+    bottom: -4px;
   }
 </style>
