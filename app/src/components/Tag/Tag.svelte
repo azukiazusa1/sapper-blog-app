ba33a849ed4030ff9e32458e5b7e7623122011d8
azukiazusa1 <cycleippei@gmail.com>
2024-05-09 08:55:09 +0900

    format

--- app/src/components/Tag/Tag.svelte
+++ app/src/components/Tag/Tag.svelte
@@ -5,7 +5,7 @@
 
 <a href={`/tags/${slug}`}>
   <span
-    class="mb-2 mr-2 inline-block rounded-fullpx-3 py-1 text-xs font-semibold no-underline hover:underline border dark:border-zinc-700"
+    class="rounded-fullpx-3 mb-2 mr-2 inline-block border py-1 text-xs font-semibold no-underline hover:underline dark:border-zinc-700"
   >
     {name}
     <slot />
