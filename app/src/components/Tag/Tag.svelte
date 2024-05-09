a4f4fc811c8680515f1054c7b63397c0c5e93d37
azukiazusa1 <59350345+azukiazusa1@users.noreply.github.com>
2024-05-09 14:08:38 +0900

    tag style (#1098)
    * tag style
    
    * format
    
    * Fix class name typo in Tag.svelte component
    
    * Refactor class name in Tag.svelte component
    
    * Refactor Tag component in tags page

--- app/src/components/Tag/Tag.svelte
+++ app/src/components/Tag/Tag.svelte
@@ -5,7 +5,7 @@
 
 <a href={`/tags/${slug}`}>
   <span
-    class="mb-2 mr-2 inline-block rounded-full bg-gray-200 px-3 py-1 text-xs font-semibold no-underline hover:underline dark:bg-gray-500"
+    class="mb-2 mr-2 inline-block rounded-full border px-3 py-1 text-xs font-semibold no-underline hover:underline dark:border-zinc-700"
   >
     {name}
     <slot />
--- app/src/routes/tags/+page.svelte
+++ app/src/routes/tags/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import Tag from "../../components/Tag/Tag.svelte";
-  import Badge from "../../components/Badge/Badge.svelte";
   import type { PageData } from "./$types";
   import variables from "$lib/variables";
 
@@ -17,8 +16,6 @@
 
 <div class="container my-8 md:mx-auto md:px-12">
   {#each data.tagCollection.items as tag (tag.slug)}
-    <Tag name={tag.name} slug={tag.slug}>
-      <Badge>{tag.linkedFrom.entryCollection.total}</Badge>
-    </Tag>
+    <Tag name={tag.name} slug={tag.slug}></Tag>
   {/each}
 </div>
