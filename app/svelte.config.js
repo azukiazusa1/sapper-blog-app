9ee31a86a83c709ebbcca59513d547f67ad67dd8
azukiazusa1 <cycleippei@gmail.com>
2024-06-23 17:58:14 +0900

    f

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -11,7 +11,7 @@ const config = {
     adapter: process.env.VERCEL
       ? adapter()
       : adapter({
-          pages: "./vercel/output/static",
+          pages: "./.vercel/output/static",
         }),
     env: {
       dir: "../",
