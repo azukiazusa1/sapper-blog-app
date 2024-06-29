bfe79c8e19cfb24ea74ca01f1c8d552e7032a1b6
azukiazusa1 <59350345+azukiazusa1@users.noreply.github.com>
2024-06-29 23:08:45 +0900

    use adapter-cloudflare (#1169)

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-cloudflare";
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +8,9 @@ const config = {
   preprocess: preprocess(),
 
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      fallback: "spa",
+    }),
     env: {
       dir: "../",
     },
