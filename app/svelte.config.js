74e43df4301750b0d29a20363a65757fa6433140
azukiazusa1 <cycleippei@gmail.com>
2025-12-06 22:13:02 +0900

    runes: true

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -7,6 +7,10 @@ const config = {
   // for more information about preprocessors
   preprocess: preprocess(),
 
+  compilerOptions: {
+    runes: true,
+  },
+
   kit: {
     adapter: adapter({
       fallback: "spa",
